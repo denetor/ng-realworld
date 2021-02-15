162bd060bc83e01f68a291875c75cd0eea88e8d8
Nicola Da Rold <nicola.darold@gmail.com>
2021-02-16 00:53:39 +0100

    aded sidenav togglable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+    sidemenuOpened: boolean = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {NavbarModule} from './core/navbar/navbar.module';
+import { NavbarModule } from './core/navbar/navbar.module';
+import { SidemenuModule } from './core/sidemenu/sidemenu.module';
+import {MatSidenavHarness} from '@angular/material/sidenav/testing';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -14,7 +18,11 @@ import {NavbarModule} from './core/navbar/navbar.module';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        NavbarModule
+        MatSidenavModule,
+        MatCheckboxModule,
+        FormsModule,
+        NavbarModule,
+        SidemenuModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
