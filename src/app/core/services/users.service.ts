577043b63ce025f478d8550cd77b848fd44a7673
Nicola Da Rold <nicola.darold@gmail.com>
2021-02-21 16:58:26 +0100

    full UsersService

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -10,7 +11,47 @@ export class UsersService {
 
     constructor(private http: HttpClient) {}
 
-    getMyself() {
+
+    public getAll(): Observable<any> {
+        return this.http.get(environment.apiUrl + this.route);
+    }
+
+
+    getMyself(): Observable<any> {
         return this.http.get(environment.apiUrl + '/' + this.route + '/myself');
+    }
+
+
+    public getOne(id): Observable<any> {
+        return this.http.get(environment.apiUrl + this.route + '/' + id);
+    }
+
+
+    public getOneByEmail(email): Observable<any> {
+        return this.http.get(environment.apiUrl + this.route + '/by-email/' + email);
+    }
+
+
+    public findOneByResetPasswordToken(token): Observable<any> {
+        return this.http.get(environment.apiUrl + this.route + '/by-resetpasswordtoken/' + token);
+    }
+
+
+    public findBySearchText(searchText): Observable<any> {
+        if (searchText.trim().length >= 2) {
+            return this.http.get(environment.apiUrl + this.route + '/by-searchtext/' + searchText);
+        } else {
+            return new Observable<any>();
+        }
+    }
+
+
+    public insert(entity): Observable<any> {
+        return this.http.post(environment.apiUrl + this.route, entity);
+    }
+
+
+    public update(entity): Observable<any> {
+        return this.http.patch(environment.apiUrl + this.route + '/' + entity.id, entity);
     }
 }
